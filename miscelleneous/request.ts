644a361ac76143af91dcedfe95ffd8e01a2a1c1c
Emmanuel Chilombo <68221880+Emmanuel355@users.noreply.github.com>
2023-11-29 06:26:36 +0200

    Update request.ts

--- miscelleneous/request.ts
+++ miscelleneous/request.ts
@@ -1,6 +1,7 @@
 
 
 export async function makeRequest(url: string, method: string, data: object){
+    //used to make an http request and returns json
     if (method === 'GET'){
         const res = await fetch(url, {method: method})
         return await res.json()
